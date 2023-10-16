1958a8a6c5620a1159b1489d99ef830fd58f5890
unknown <dkvz@naver.com>
2023-10-16 17:30:25 +0900

    반복 for줄이기. substr대신 slice

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,6 @@
 function problem2(cryptogram) {
-  let 문자열 = String(cryptogram)
+  let 문자열 = String(cryptogram);
+  let 길이 = 문자열.length;
   var answer;
 
 // 매개변수의 각 글자를 비교하여 같으면 둘다 없애기
@@ -8,30 +9,26 @@ function problem2(cryptogram) {
 // 반복하여 연속하는 중복 문자가 더 없으면 그 결과 값을 answer에 부여 하여 return 하기;
 // ㄴ 반복하는 회수는 cryptogram의 글자 길이 만큼
 
-for(var i = 0; i<문자열.length;i++){
+for(var i = 0; i<길이;i++){
     if(문자열[i] == 문자열[i+1]){
-       문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
-       answer = 문자열
-       for(var i = 0; i<문자열.length;i++){
+       문자열 = 문자열.slice(0,i) + 문자열.slice(i+2);
+       answer = 문자열;
+       for(var i = 0; i<길이;i++){
            if(문자열[i] == 문자열[i+1]){
-              문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
-              console.log(2)
-             answer = 문자열
-             for(var i = 0; i<문자열.length;i++){
+              문자열 = 문자열.slice(0,i) + 문자열.slice(i+2);
+              answer = 문자열;
+             for(var i = 0; i<길이;i++){
                 if(문자열[i] == 문자열[i+1]){
-                문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
-                console.log(3)
-                answer = 문자열
-                for(var i = 0; i<문자열.length;i++){
+                문자열 = 문자열.slice(0,i) + 문자열.slice(i+2);
+                answer = 문자열;
+                for(var i = 0; i<길이;i++){
                   if(문자열[i] == 문자열[i+1]){
-                    문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
-                    console.log(4)
-                    answer = 문자열
-                    for(var i = 0; i<문자열.length;i++){
+                    문자열 = 문자열.slice(0,i) + 문자열.slice(i+2);
+                    answer = 문자열;
+                    for(var i = 0; i<길이;i++){
                   if(문자열[i] == 문자열[i+1]){
-                    문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
-                    console.log(5)
-                    answer = 문자열
+                    문자열 = 문자열.slice(0,i) + 문자열.slice(i+2);
+                    answer = 문자열;
                   }}
                   }}}
        
