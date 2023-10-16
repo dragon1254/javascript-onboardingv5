53b30d8d2d5c594c66bd52fbdf7cb88c9e21c2e2
unknown <dkvz@naver.com>
2023-10-16 17:18:28 +0900

    문제2 끝. 중첩 줄이기 생각해야함

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,48 @@
 function problem2(cryptogram) {
+  let 문자열 = String(cryptogram)
   var answer;
+
+// 매개변수의 각 글자를 비교하여 같으면 둘다 없애기
+// cryptogram[0]==cryptogram[1]이면 
+// 두항을 삭제하고 새로운 문자열 만들기
+// 반복하여 연속하는 중복 문자가 더 없으면 그 결과 값을 answer에 부여 하여 return 하기;
+// ㄴ 반복하는 회수는 cryptogram의 글자 길이 만큼
+
+for(var i = 0; i<문자열.length;i++){
+    if(문자열[i] == 문자열[i+1]){
+       문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
+       answer = 문자열
+       for(var i = 0; i<문자열.length;i++){
+           if(문자열[i] == 문자열[i+1]){
+              문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
+              console.log(2)
+             answer = 문자열
+             for(var i = 0; i<문자열.length;i++){
+                if(문자열[i] == 문자열[i+1]){
+                문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
+                console.log(3)
+                answer = 문자열
+                for(var i = 0; i<문자열.length;i++){
+                  if(문자열[i] == 문자열[i+1]){
+                    문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
+                    console.log(4)
+                    answer = 문자열
+                    for(var i = 0; i<문자열.length;i++){
+                  if(문자열[i] == 문자열[i+1]){
+                    문자열 = 문자열.substr(0,i) + 문자열.substr(i+2)
+                    console.log(5)
+                    answer = 문자열
+                  }}
+                  }}}
+       
+    }
+    }  
+    }  
+    }
+}
+
+
+
   return answer;
 }
 
