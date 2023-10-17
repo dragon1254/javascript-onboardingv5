b5b7932d514f397e7866d606152a00a84ab0022f
unknown <dkvz@naver.com>
2023-10-17 17:00:01 +0900

    문제5 끝 function써서 줄임

--- src/problem5.js
+++ src/problem5.js
@@ -5,60 +5,77 @@ function problem5(money) {
 // 남은 것에서 10000보다 큰지 확인하고 크면 50000 빼고 10000쪽에 카운트 1올림
 // 반복하여 일원동전까지 하고 각 카운트를 배열로 담아 answer에 대입
 
-  let 오만 = 0;
-  let 만 = 0;
-  let 오천 = 0;
-  let 천 = 0;
-  let 오백 = 0;
-  let 백 = 0;
-  let 오십 = 0;
-  let 십 = 0;
-  let 일 = 0;
+//   let 오만 = 0;
+//   let 만 = 0;
+//   let 오천 = 0;
+//   let 천 = 0;
+//   let 오백 = 0;
+//   let 백 = 0;
+//   let 오십 = 0;
+//   let 십 = 0;
+//   let 일 = 0;
   
-while(money>=50000){
-  money = money - 50000
-  오만++
-}
-while(money>=10000){
-  money = money-10000
-  만++
-}
-while(money>=5000){
-  money = money-5000
-  오천++
-}
-while(money>=1000){
-  money = money-1000
-  천++
-}
-while(money>=500){
-  money = money-500
-  오백++
-}
-while(money>=100){
-  money = money-100
-  백++
-}
-while(money>=50){
-  money = money-50
-  오십++
-}
-while(money>=10){
-  money = money-10
-  십++
-}
-while(money>=1){
-  money = money-1
-  일++
-}
-answer=[오만,만,오천,천,오백,백,오십,십,일]
+// while(money>=50000){
+//   money = money - 50000
+//   오만++
+// }
+// while(money>=10000){
+//   money = money-10000
+//   만++
+// }
+// while(money>=5000){
+//   money = money-5000
+//   오천++
+// }
+// while(money>=1000){
+//   money = money-1000
+//   천++
+// }
+// while(money>=500){
+//   money = money-500
+//   오백++
+// }
+// while(money>=100){
+//   money = money-100
+//   백++
+// }
+// while(money>=50){
+//   money = money-50
+//   오십++
+// }
+// while(money>=10){
+//   money = money-10
+//   십++
+// }
+// while(money>=1){
+//   money = money-1
+//   일++
+// }
+// answer=[오만,만,오천,천,오백,백,오십,십,일]
 
+let count =0;
+let answerArray =[]
 
-
-
-
-  return answer;
+function 돈(a){
+    count = 0;
+    while(money>=a){
+        money = money - a;
+        count++
+    }
+answerArray.push(count)
 }
 
+돈(50000);
+돈(10000);
+돈(5000);
+돈(1000);
+돈(500);
+돈(100);
+돈(50);
+돈(10);
+돈(1);
+answer = answerArray
+return answer;
+}
 
 module.exports = problem5;
