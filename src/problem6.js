c8ca2c53faf65fc531228cf55204f6d400447f66
unknown <dkvz@naver.com>
2023-10-17 17:34:45 +0900

    문제6 고민중

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,26 @@
 function problem6(forms) {
   var answer;
+
+// 닉네임 찾아봄 - forms 배열에서 닉네임쪽 
+// 해결 방법
+// indexOf와 lastIndexOf 함수를 함께 사용해야 한다.
+// indexOf로 문자열에서 특정 문자가 처음 등장하는 index를 구하고, lastIndexOf로 뒤에서 부터 등장하는 index를 구해 일치하지 않으면 중복된 문자가 존재함을 알 수 있다.
+
+// function isUnique(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+// 까지 한번 찾아보기
+// ㄴ javascript 일부 문자 중복 체크 로 검색 했음
+
+
+
+
   return answer;
 }
 
